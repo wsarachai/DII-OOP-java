00e971c509cef4f1bee2aa5db41ad7831263e248
Watcharin Sarachai <sarachaii@gmail.com>
2022-04-18 09:50:05 +0700

    init commit for lesson 09 update for student

--- app/src/main/java/org/dii/oop/lesson09/exercise01/Lesson.java
+++ app/src/main/java/org/dii/oop/lesson09/exercise01/Lesson.java
@@ -1,25 +1,7 @@
 package org.dii.oop.lesson09.exercise01;
 
-import javax.swing.*;
-
-public class Lesson extends JFrame {
-    protected final int FPS = 1000/120;
-
-    public Lesson() {
-        super("Duck Simulator");
-        DuckSimulator duckSimulatorPanel = new DuckSimulator();
-
-        add(duckSimulatorPanel);
-
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        pack();
-
-        Timer timer = new Timer(FPS, duckSimulatorPanel);
-        timer.start();
-    }
-
+public class Lesson {
     public static void run() {
-        Lesson lesson = new Lesson();
-        lesson.setVisible(true);
+
     }
 }
--- app/src/main/java/org/dii/oop/lesson09/exercise03/Lesson.java
+++ app/src/main/java/org/dii/oop/lesson09/exercise03/Lesson.java
@@ -2,22 +2,6 @@ package org.dii.oop.lesson09.exercise03;
 
 public class Lesson {
     public static void run() {
-        Beverage beverage = new Espresso();
-        System.out.println(beverage.getDescription()
-                + " $" + beverage.getCost());
 
-        Beverage beverage2 = new DarkRoast();
-        beverage2 = new Mocha(beverage2);
-        beverage2 = new Mocha(beverage2);
-        beverage2 = new Whip(beverage2);
-        System.out.println(beverage2.getDescription()
-                + " $" + beverage2.getCost());
-
-        Beverage beverage3 = new HouseBlend();
-        beverage3 = new Soy(beverage3);
-        beverage3 = new Mocha(beverage3);
-        beverage3 = new Whip(beverage3);
-        System.out.println(beverage3.getDescription()
-                + " $" + beverage3.getCost());
     }
 }
